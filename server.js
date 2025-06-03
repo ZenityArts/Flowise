5e5796e8b3e8f9033c3150b134d33eb73ed26331
ZenityArts <123758424+ZenityArts@users.noreply.github.com>
2025-06-03 18:19:21 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,16 +1,12 @@
 const express = require('express');
-const fs = require('fs');
-const path = require('path');
 const app = express();
+const PORT = process.env.PORT || 3000;
 
 app.get('/getfile/:filename', (req, res) => {
-    const filename = req.params.filename;
-    const filePath = path.join('/mnt/data', filename);
-    if (!fs.existsSync(filePath)) {
-        return res.status(404).send("File not found");
-    }
-    const content = fs.readFileSync(filePath, 'utf8');
-    res.send(content);
+  const filePath = `/mnt/data/${req.params.filename}`;
+  res.sendFile(filePath);
 });
 
-module.exports = app;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
